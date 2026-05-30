082c81deb2d64d1b7cb9dd47f7a3f12beb3b3264
Carlos Villanueva <luisvillanuevaandrades@gmail.com>
2026-05-30 04:19:18 -0400

    fix imports page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
-import { SITE_CONFIG } from '../constants';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import { SITE_CONFIG } from './constants';
 
 export default function TermsAndConditions() {
   return (
